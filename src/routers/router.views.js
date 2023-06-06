b2ae78d6ed712cb935ffad42055875cdf850b2f5
Manu.camilo3 <Manu.camilo3@outlook.com>
2023-06-06 00:14:33 -0300

    feat: login render OK

--- src/routers/router.views.js
+++ src/routers/router.views.js
@@ -17,55 +17,55 @@ router.get("/realtimeProducts", (request, response) => {
 });
 
 router.get("/products", async (request, response) => {
-    try {
-        const { limit = 10, page = 1, sort, query, filter } = request.query;
-    
-        let sortValue = 0;
-        if (sort === "desc") {
-          sortValue = -1;
-        } else if (sort === "asc") {
-          sortValue = 1;
-        }
-    
-        const queryFilter = {};
-        if (query === "title") {
-          queryFilter.title = filter;
-        } else if (query === "category") {
-          queryFilter.category = filter;
-        } 
-
-        const result = await productModel.paginate(queryFilter, {
-            page:parseInt(page),
-            limit: parseInt(limit),
-            sort: sortValue !== 0 ? { price: sortValue } : undefined,
-            lean:true
-        });
-        const { docs, totalPages, prevPage, nextPage, hasPrevPage, hasNextPage } = result;
-
-        const user = request.session.user;
-
-        response.render('productList', {
-          products: docs,
-          user: {
-            first_name: user.first_name,
-            last_name: user.last_name,
-            email: user.email,
-            role: request.session.role
-          },
-          totalPages: totalPages,
-          prevPage: prevPage,
-          nextPage: nextPage,
-          page: page,
-          hasPrevPage: hasPrevPage,
-          hasNextPage: hasNextPage,
-          prevLink: hasPrevPage ? `/products?page=${prevPage}&limit=${limit}&sort=${sort}&query=${query}&filter=${filter}` : '',
-          nextLink: hasNextPage ? `/products?page=${nextPage}&limit=${limit}&sort=${sort}&query=${query}&filter=${filter}` : ''
-        });
-      } catch (error) {
-        console.error("Error al obtener los productos:", error);
-        response.status(500).send("Error interno del servidor");
+  try {
+      const { limit = 10, page = 1, sort, query, filter } = request.query;
+  
+      let sortValue = 0;
+      if (sort === "desc") {
+        sortValue = -1;
+      } else if (sort === "asc") {
+        sortValue = 1;
       }
-    });
+  
+      const queryFilter = {};
+      if (query === "title") {
+        queryFilter.title = filter;
+      } else if (query === "category") {
+        queryFilter.category = filter;
+      } 
+
+      const result = await productModel.paginate(queryFilter, {
+          page:parseInt(page),
+          limit: parseInt(limit),
+          sort: sortValue !== 0 ? { price: sortValue } : undefined,
+          lean:true
+      });
+      const { docs, totalPages, prevPage, nextPage, hasPrevPage, hasNextPage } = result;
+
+      const user = request.session.user;
+
+      response.render('productList', {
+        products: docs,
+        user: {
+          first_name: user.first_name,
+          last_name: user.last_name,
+          email: user.email,
+          role: request.session.role
+        },
+        totalPages: totalPages,
+        prevPage: prevPage,
+        nextPage: nextPage,
+        page: page,
+        hasPrevPage: hasPrevPage,
+        hasNextPage: hasNextPage,
+        prevLink: hasPrevPage ? `/products?page=${prevPage}&limit=${limit}&sort=${sort}&query=${query}&filter=${filter}` : '',
+        nextLink: hasNextPage ? `/products?page=${nextPage}&limit=${limit}&sort=${sort}&query=${query}&filter=${filter}` : ''
+      });
+    } catch (error) {
+      console.error("Error al obtener los productos:", error);
+      response.status(500).send("Error interno del servidor");
+    }
+  });
 
 router.get("/carts/:cid", async (request, response) => {
   const cid = request.params.cid
