94ca56ced82f3c1eb002bc85e7bbf7f31ee7936a
Manu.camilo3 <Manu.camilo3@outlook.com>
2023-07-25 14:18:17 -0300

    feat: Ticket Model

--- src/routers/router.cart.js
+++ src/routers/router.cart.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import cartController from '../controllers/cartController.js';
+import isAdmin from '../services/role.js';
 
 const router = Router();
 
 
 router.post('/', cartController.pcCreateCart);
 router.get('/:cid', cartController.pcGetCart);
-router.post('/:cid/products/:pid', cartController.pcAddProductToCart);
+router.post('/:cid/products/:pid', !isAdmin, cartController.pcAddProductToCart);
 router.delete('/:cid/products/:pid', cartController.pcDeleteProductToCart);
 router.put('/:cid',cartController.pcUpdateCart);
 router.put('/:cid/products/:pid',cartController.pcUpdateQuantity) 
