42b57e57baa0e77bcfb2e32c9fe07e771be2a9e6
Manu.camilo3 <Manu.camilo3@outlook.com>
2023-08-12 21:06:49 -0300

    Desarrollo Recover Password.router.

--- src/routers/router.recover.js
+++ src/routers/router.recover.js
@@ -1,3 +1,10 @@
 import express from 'express';
 
-const recoverRouter = express.Router();
+const router = express.Router();
+
+router.get('/',) // 1- mostrar el formulario - redireccionado desde Forgout your password?
+router.post('/sendRecoveryEmail') // Enviar email de recuperación, al email indicado. Tambien corrobora correo existente.
+router.get('/reset/:token') // mostrar formulario restablecer la contraseña,  Luego de clickear recuperar en el correo
+router.post('/reset/:token') // ingresa su nueva contraseña y envía el formulario, esta ruta  actualiza la contraseña del usuario.
+router.get('/success') // mostrar página - contraseña restablecida -
+router.get('/error') // 
